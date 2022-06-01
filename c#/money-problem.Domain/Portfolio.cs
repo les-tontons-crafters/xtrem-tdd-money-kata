e1fd45e91fc620b80518d7079206c7c1f5d89a3b
Tr00d <guillaume.faas@live.fr>
2022-06-01 08:50:21 +0200

    Remove for loop, use datastructure to hold state

--- c#/money-problem.Domain/Portfolio.cs
+++ c#/money-problem.Domain/Portfolio.cs
@@ -8,26 +8,51 @@ public class Portfolio
 
     public Money Evaluate(Bank bank, Currency currency)
     {
-        double convertedResult = 0;
-        var missingExchangeRates = new List<MissingExchangeRateException>();
-        foreach (Money money in this.moneys)
+        List<ConversionResult> results = this.moneys
+            .Select(money => ConvertMoney(bank, currency, money))
+            .ToList();
+        return results.Any(result => result.HasException())
+            ? throw new MissingExchangeRatesException(results
+                .Where(result => result.HasException())
+                .Select(result => result.GetExceptionUnsafe())
+                .ToList())
+            : new Money(results.Sum(result => result.GetMoneyUnsafe().Amount), currency);
+    }
+
+    private static ConversionResult ConvertMoney(Bank bank, Currency currency, Money money)
+    {
+        try
         {
-            try
-            {
-                Money convertedMoney = bank.Convert(money, currency);
-                convertedResult += convertedMoney.Amount;
-            }
-            catch (MissingExchangeRateException exception)
-            {
-                missingExchangeRates.Add(exception);
-            }
+            return new ConversionResult(bank.Convert(money, currency));
         }
-        
-        if (missingExchangeRates.Any()) {
-            throw new MissingExchangeRatesException(missingExchangeRates);
+        catch (MissingExchangeRateException exception)
+        {
+            return new ConversionResult(exception);
         }
-        
-        // Simply instantiate a new Money from here
-        return new Money(convertedResult, currency);
+    }
+
+    private class ConversionResult
+    {
+        private readonly MissingExchangeRateException? exception;
+
+        private readonly Money? money;
+
+        public ConversionResult(Money money)
+        {
+            this.money = money;
+        }
+
+        public ConversionResult(MissingExchangeRateException exception)
+        {
+            this.exception = exception;
+        }
+
+        public bool HasMoney() => this.money != null;
+
+        public bool HasException() => this.exception != null;
+
+        public MissingExchangeRateException GetExceptionUnsafe() => this.exception!;
+
+        public Money GetMoneyUnsafe() => this.money!;
     }
 }
