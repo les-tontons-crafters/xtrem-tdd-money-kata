0e5e90a38d590808025275a96bbe90a6f793a16c
Tr00d <guillaume.faas@live.fr>
2022-06-02 08:16:00 +0200

    Refactor Portfolio.cs - expressing intention in the code

--- c#/money-problem.Domain/Portfolio.cs
+++ c#/money-problem.Domain/Portfolio.cs
@@ -8,16 +8,28 @@ public class Portfolio
 
     public Money Evaluate(Bank bank, Currency currency)
     {
-        List<ConversionResult> results = this.moneys
+        List<ConversionResult> results = this.GetConvertedMoneys(bank, currency);
+        return ContainsFailure(results)
+            ? throw ToException(results)
+            : ToMoney(results, currency);
+    }
+
+    private static MissingExchangeRatesException ToException(IEnumerable<ConversionResult> results) =>
+        new(results
+            .Where(result => result.HasException())
+            .Select(result => result.GetExceptionUnsafe())
+            .ToList());
+
+    private static Money ToMoney(IEnumerable<ConversionResult> results, Currency currency) =>
+        new(results.Sum(result => result.GetMoneyUnsafe().Amount), currency);
+
+    private static bool ContainsFailure(IEnumerable<ConversionResult> results) =>
+        results.Any(result => result.HasException());
+
+    private List<ConversionResult> GetConvertedMoneys(Bank bank, Currency currency) =>
+        this.moneys
             .Select(money => ConvertMoney(bank, currency, money))
             .ToList();
-        return results.Any(result => result.HasException())
-            ? throw new MissingExchangeRatesException(results
-                .Where(result => result.HasException())
-                .Select(result => result.GetExceptionUnsafe())
-                .ToList())
-            : new Money(results.Sum(result => result.GetMoneyUnsafe().Amount), currency);
-    }
 
     private static ConversionResult ConvertMoney(Bank bank, Currency currency, Money money)
     {
