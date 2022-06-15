8f4a03635155af41f77fd314e215559c8d7a7e33
FAAS Guillaume <guillaume.faas@live.fr>
2022-06-15 06:30:13 +0200

    Fix merge conflicts

--- c#/money-problem.Domain/MoneyExtensions.cs
+++ c#/money-problem.Domain/MoneyExtensions.cs
@@ -4,13 +4,7 @@ public static class MoneyExtensions
 {
     public static Money Euros(this double amount) => new(amount, Currency.EUR);
 
-    public static Money Euros(this int amount) => new(amount, Currency.EUR);
-
     public static Money Dollars(this double amount) => new(amount, Currency.USD);
 
-    public static Money Dollars(this int amount) => new(amount, Currency.USD);
-    
     public static Money KoreanWons(this double amount) => new(amount, Currency.KRW);
-
-    public static Money KoreanWons(this int amount) => new(amount, Currency.KRW);
 }
