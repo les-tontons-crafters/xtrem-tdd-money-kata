6374e34284a1f5645966581dc34a97e71df1cb80
Yoan Thirion <yoan.thirion@gmail.com>
2022-06-24 17:54:52 +0200

    final scala version for no Money

--- scala/src/main/scala/domain/Portfolio.scala
+++ scala/src/main/scala/domain/Portfolio.scala
@@ -2,33 +2,29 @@ package domain
 
 import domain.Currency.Currency
 
-import scala.collection.mutable
-
 class Portfolio() {
-  private val moneys: mutable.Map[Currency, List[Double]] =
-    mutable.Map.empty.withDefault(_ => List.empty)
+  private var moneys: List[Money] = List.empty
 
-  def add(amount: Double, currency: Currency): Unit =
-    moneys(currency) = moneys(currency) :+ amount
+  def add(money: Money): Unit =
+    moneys = moneys :+ money
 
-  def evaluate(bank: Bank, toCurrency: Currency): Double = {
+  def evaluate(bank: Bank, toCurrency: Currency): Money = {
     var convertedResult = 0d
     var missingExchangeRates: Seq[MissingExchangeRateException] = Seq.empty
 
     for (money <- moneys) {
-      for (amount <- money._2) {
-        try {
-          val convertedAmount = bank.convert(amount, money._1, toCurrency)
-          convertedResult += convertedAmount
-        } catch {
-          case missingExchangeRate: MissingExchangeRateException =>
-            missingExchangeRates = missingExchangeRates :+ missingExchangeRate
-        }
+      try {
+        val convertedAmount = bank.convert(money, toCurrency)
+        convertedResult += convertedAmount.amount
+      } catch {
+        case missingExchangeRate: MissingExchangeRateException =>
+          missingExchangeRates = missingExchangeRates :+ missingExchangeRate
       }
     }
 
     if (missingExchangeRates.nonEmpty)
       throw MissingExchangeRatesException(missingExchangeRates.toSeq)
-    convertedResult
+
+    Money(convertedResult, toCurrency)
   }
 }
--- scala/src/test/scala/domain/PortfolioShould.scala
+++ scala/src/test/scala/domain/PortfolioShould.scala
@@ -1,6 +1,7 @@
 package domain
 
 import domain.Currency._
+import domain.DomainExtensions.MoneyExtensions
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.funsuite.AnyFunSuite
 
@@ -14,44 +15,44 @@ class PortfolioShould extends AnyFunSuite with BeforeAndAfterEach {
 
   test("5 USD + 10 USD = 15 USD") {
     val portfolio = new Portfolio()
-    portfolio.add(5, USD)
-    portfolio.add(10, USD)
+    portfolio.add(5.dollars())
+    portfolio.add(10.dollars())
 
-    assert(portfolio.evaluate(bank, USD) == 15)
+    assert(portfolio.evaluate(bank, USD) == 15.dollars())
   }
 
   test("5 USD + 10 EUR = 17 USD") {
     val portfolio = new Portfolio()
-    portfolio.add(5, USD)
-    portfolio.add(10, EUR)
+    portfolio.add(5.dollars())
+    portfolio.add(10.euros())
 
-    assert(portfolio.evaluate(bank, USD) == 17)
+    assert(portfolio.evaluate(bank, USD) == 17.dollars())
   }
 
   test("1 USD + 1100 KRW = 2200 KRW") {
     val portfolio = new Portfolio()
-    portfolio.add(1, USD)
-    portfolio.add(1100, KRW)
+    portfolio.add(1.dollars())
+    portfolio.add(1100.koreanWons())
 
-    assert(portfolio.evaluate(bank, KRW) == 2200)
+    assert(portfolio.evaluate(bank, KRW) == 2200.koreanWons())
   }
 
   test("5 USD + 10 EUR + 4 EUR = 21.8 USD") {
     val portfolio = new Portfolio()
-    portfolio.add(5, USD)
-    portfolio.add(10, EUR)
-    portfolio.add(4, EUR)
+    portfolio.add(5.dollars())
+    portfolio.add(10.euros())
+    portfolio.add(4.euros())
 
-    assert(portfolio.evaluate(bank, USD) == 21.8)
+    assert(portfolio.evaluate(bank, USD) == 21.8.dollars())
   }
 
   test(
     "Throws a MissingExchangeRatesException in case of missing exchange rates"
   ) {
     val portfolio = new Portfolio()
-    portfolio.add(1, EUR)
-    portfolio.add(1, USD)
-    portfolio.add(1, KRW)
+    portfolio.add(1.euros())
+    portfolio.add(1.dollars())
+    portfolio.add(1.koreanWons())
 
     val exception =
       intercept[MissingExchangeRatesException](portfolio.evaluate(bank, EUR))
