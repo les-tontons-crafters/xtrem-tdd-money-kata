a785c552d3f17998354be1c40a9f8dd7831b5004
Tr00d <guillaume.faas@live.fr>
2022-07-07 13:31:13 +0200

    Finish step 7 to introduce LanguageExt

--- c#/money-problem.Domain/Bank.cs
+++ c#/money-problem.Domain/Bank.cs
@@ -6,31 +6,25 @@ namespace money_problem.Domain
 {
     public sealed class Bank
     {
-        private readonly ReadOnlyDictionary<string, double> _exchangeRates;
+        private readonly Map<string, double> _exchangeRates;
 
-        private Bank(IDictionary<string, double> exchangeRates) => _exchangeRates = new ReadOnlyDictionary<string, double>(exchangeRates);
+        private Bank(Map<string, double> exchangeRates) => this._exchangeRates = new Map<string, double>(exchangeRates);
 
         public static Bank WithExchangeRate(Currency from, Currency to, double rate) => 
-            new Bank(new Dictionary<string, double>()).AddExchangeRate(from, to, rate);
+            new Bank(new Map<string, double>()).AddExchangeRate(from, to, rate);
 
-        public Bank AddExchangeRate(Currency from, Currency to, double rate)
-        {
-            var updatedRates = new Dictionary<string, double>(this._exchangeRates);
-            var key = KeyFor(from, to);
-            updatedRates.TryAdd(key, default);
-            updatedRates[key] = rate;
-            return new Bank(updatedRates);
-        }
+        public Bank AddExchangeRate(Currency from, Currency to, double rate) =>
+            new Bank(new Map<string, double>(this._exchangeRates).AddOrUpdate(KeyFor(from, to), rate));
 
         private static string KeyFor(Currency from, Currency to) => $"{from}->{to}";
         
         private Money ConvertSafely(Money money, Currency to) =>
             to == money.Currency
                 ? money
-                : money with { Amount = money.Amount * _exchangeRates[KeyFor(money.Currency, to)], Currency = to};
+                : money with { Amount = money.Amount * this._exchangeRates.Find(KeyFor(money.Currency, to)).IfNone(0), Currency = to};
 
         private bool CanConvert(Currency from, Currency to) =>
-                from == to || _exchangeRates.ContainsKey(KeyFor(from, to));
+                from == to || this._exchangeRates.ContainsKey(KeyFor(from, to));
 
         public Either<string, Money> Convert(Money money, Currency currency) =>
             this.CanConvert(money.Currency, currency)
--- c#/money-problem.Domain/Portfolio.cs
+++ c#/money-problem.Domain/Portfolio.cs
@@ -1,46 +1,14 @@
-using System.Collections.Immutable;
 using LanguageExt;
 
 namespace money_problem.Domain;
 
 public class Portfolio
 {
-    private readonly ICollection<Money> moneys;
+    private readonly Seq<Money> moneys;
 
-    public Portfolio()
-    {
-        this.moneys = new List<Money>();
-    }
+    public Portfolio() => this.moneys = Seq<Money>.Empty;
 
-    private Portfolio(IEnumerable<Money> moneys)
-    {
-        this.moneys = moneys.ToImmutableList();
-    }
-
-    private static bool ContainsFailure(IEnumerable<Either<string, Money>> results) =>
-        results.Any(result => result.IsLeft);
-
-    private List<Either<string, Money>> GetConvertedMoneys(Bank bank, Currency currency) =>
-        this.moneys
-            .Select(money => bank.Convert(money, currency))
-            .ToList();
-
-    public Portfolio Add(Money money)
-    {
-        List<Money> updatedMoneys = this.moneys.ToList();
-        updatedMoneys.Add(money);
-        return new Portfolio(updatedMoneys);
-    }
-
-    private static string GetMissingRates(IEnumerable<string> missingRates) => missingRates
-        .Select(value => $"[{value}]")
-        .Aggregate((r1, r2) => $"{r1},{r2}");
-
-    private string ToFailure(IEnumerable<Either<string, Money>> results) =>
-        $"Missing exchange rate(s): {GetMissingRates(results.Where(result => result.IsLeft).Select(result => result.IfRight(string.Empty)))}";
-
-    private Money ToSuccess(IEnumerable<Either<string, Money>> results, Currency currency) =>
-        new(results.Where(result => result.IsRight).Sum(result => result.IfLeft(Money.Empty(currency)).Amount), currency);
+    private Portfolio(Seq<Money> moneys) => this.moneys = moneys;
 
     public Either<string, Money> Evaluate(Bank bank, Currency currency)
     {
@@ -49,4 +17,27 @@ public class Portfolio
             ? Either<string, Money>.Left(this.ToFailure(results))
             : Either<string, Money>.Right(this.ToSuccess(results, currency));
     }
+
+    private List<Either<string, Money>> GetConvertedMoneys(Bank bank, Currency currency) =>
+        this.moneys
+            .Select(money => bank.Convert(money, currency))
+            .ToList();
+
+    private static bool ContainsFailure(IEnumerable<Either<string, Money>> results) =>
+        results.Any(result => result.IsLeft);
+
+    private string ToFailure(IEnumerable<Either<string, Money>> results) =>
+        $"Missing exchange rate(s): {GetMissingRates(results.Where(result => result.IsLeft).Select(result => result.IfRight(string.Empty)))}";
+
+    private static string GetMissingRates(IEnumerable<string> missingRates) => missingRates
+        .Select(value => $"[{value}]")
+        .Aggregate((r1, r2) => $"{r1},{r2}");
+
+    private Money ToSuccess(IEnumerable<Either<string, Money>> results, Currency currency) =>
+        new(results
+                .Where(result => result.IsRight)
+                .Sum(result => result.IfLeft(Money.Empty(currency)).Amount),
+            currency);
+
+    public Portfolio Add(Money money) => new(this.moneys.Add(money));
 }
