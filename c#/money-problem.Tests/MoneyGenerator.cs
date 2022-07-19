bb86b40aa1baaf98af90b69cf391a04b0ec0f9a4
FAAS Guillaume <guillaume.faas@live.fr>
2022-07-19 11:06:34 +0200

    Filter small values with money generator

--- c#/money-problem.Tests/MoneyGenerator.cs
+++ c#/money-problem.Tests/MoneyGenerator.cs
@@ -1,4 +1,5 @@
-﻿using FsCheck;
+﻿using System;
+using FsCheck;
 using money_problem.Domain;
 
 namespace money_problem.Tests;
@@ -6,10 +7,13 @@ namespace money_problem.Tests;
 public class MoneyGenerator
 {
     private const double MaxAMount = 1000000000;
-    
+    private const int MaxDigits = 5;
+
     public static Arbitrary<Money> GenerateMoneys() =>
-        Arb.From(from amount in Arb.Generate<double>()
+        Arb.From(from amount in GetAmountGenerator()
             from currency in Arb.Generate<Currency>()
-            where amount is >= 0 - MaxAMount and <= MaxAMount 
             select new Money(amount, currency));
+
+    private static Gen<double> GetAmountGenerator() => Arb.Default.Float()
+        .MapFilter(x => Math.Round(x, MaxDigits), x => x is >= 0 - MaxAMount and <= MaxAMount).Generator;
 }
